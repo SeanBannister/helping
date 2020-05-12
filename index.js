d81107f6cd6b775d9eb71654524f839e9343f0b3
SeanBannister <Sean@SeanBannister.com>
2020-05-12 23:47:56 +1000

    first commit

---
+++ index.js
@@ -0,0 +1,63 @@
+/**
+ * Check if a variable|array|object|etc is empty|null|undefined|etc. 
+ * A full list of what this function considers to be null can be seen in the tests of the Helping project.
+ * 
+ * @param {(*)} value
+ * @return {boolean} A boolean that states if the parsed in value is considered empty.
+ */
+export function empty(value) {
+  // If any of these return true this function returns true.
+  return (
+    // Null or undefined.
+    value == null ||
+    // Length is zero && it's not a function.
+    (value.length === 0 && typeof value !== "function") ||
+    // Is an Object && is empty. Just detects: {}
+    (value.constructor === Object && Object.keys(value).length === 0) ||
+    // Check if a Set() or Map() is empty
+    value.size === 0 ||
+    // Check if NaN - The only JavaScript value that is unequal to itself.
+    value !== value
+  );
+}
+
+/**
+ * Iterate over an Object|Array and return the key and value of each property. 
+ * - Doesn't iterate over prototypes.
+ * - Loops can be broken with "return false".
+ * 
+ * This was developed to overcome some of the shortcomings of other methods: https://codepen.io/SeanBannister/pen/qOoLrm
+ * 
+ * This code is a modified version of jQuery 3.5.0's $.each
+ * 
+ * @license MIT Copyright JS Foundation and other contributors, https://js.foundation/
+ *
+ * @param {(Object|Array)} obj An object we'll iterate over.
+ * @param {function(number|string, *)} callback Returns a key (index) and a value.
+ */
+export function each(obj, callback) {
+  var length, i = 0;
+  
+  // If obj is an array
+  // Note: The function from the jquery library originally had their own isArrayLike function here, I couldn't
+  //       get the extracted version working as expected so I'm using .isArray as I believe it will meet our needs.
+  if (Array.isArray(obj)) {
+    length = obj.length;
+    for (; i < length; i++) {
+      if (callback.call(obj[i], i, obj[i]) === false) {
+        break;
+      }
+    }
+  }
+  // If obj is a javascript object
+  else {
+    for (i in obj) {
+      if (obj.hasOwnProperty(i)) {
+        if (callback.call(obj[i], i, obj[i]) === false) {
+          break;
+        }
+      }
+    }
+  }
+  return obj;
+}
---
+++ readme.md
@@ -0,0 +1,35 @@
+# Helping.js
+This is a collection of generic Javascript functions that help in day to day programming. Many were collected from around the internet in which case attribution is included above the function. Please keep in mind that this collection was predominantly developed to meet my own needs.
+
+## Requirements
+
+This project uses ECMAScript 6 features that require Node 12.17 or later.
+
+## Installation
+
+**NPM :** ` npm install helping`
+
+**Yarn :** ` yarn add helping`
+
+## Usage
+
+Each function should be imported individually.
+
+``` javascript
+import { each, empty } from "helping";
+```
+
+## Documentation
+
+Documented using [JSDoc](https://jsdoc.app/) and available [here](https://doxdox.org/seanbannister/helping).
+
+## Todo
+
+- [ ] Once Mocha supports ES Modules write testing suite.
+
+## Contributing
+
+Pull requests are welcome. Please fix bugs or make improvements to existing code. I might be reluctant to add new features as this project was mostly developed to meet my own needs but feel free to open an issue to discuss.
+
+## License
+[ISC](https://choosealicense.com/licenses/isc/) - [View the license.](https://github.com/SeanBannister/helping/blob/master/LICENSE.txt)
